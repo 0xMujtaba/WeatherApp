7ac00ec6733b106ca0b64f2bc5a22453969d9d0d
Ghulame Mujtaba Arbi <58784608+steelslayer091@users.noreply.github.com>
2020-07-15 02:08:06 +0200

    Added API fetch requests

--- WeatherApp.js
+++ WeatherApp.js
@@ -1,1 +1,29 @@
+var button = document.querySelector('.button')
+var inputValue = document.querySelector('.inputValue')
+var name = document.querySelector('.name');
+var desc = document.querySelector('.desc');
+var temp = document.querySelector('.temp');
+
+button.addEventListener('click', function(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=16628912310a9e56c80b9573a1903c86&units=metric')
+    .then(response => response.json())
+    .then(data => {
+        var nameValue = data['name'];
+        var tempValue = data['main']['temp'];
+        var descValue = data['weather'][0]['description'];
+
+        tempValue = tempValue + '° Celcius';
+
+        name.innerHTML = nameValue;
+        temp.innerHTML = tempValue;
+        desc.innerHTML = descValue;
+    })
+
+
+.catch(err => alert("Invalid city name"))
+
+
+
+
+})
 
