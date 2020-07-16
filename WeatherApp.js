f43758cd9c936fc9af2495955829cc4ed6f9c329
Ghulame Mujtaba Arbi <58784608+steelslayer091@users.noreply.github.com>
2020-07-16 16:24:44 +0200

    Added clock script

--- WeatherApp.js
+++ WeatherApp.js
@@ -4,6 +4,8 @@ var name = document.querySelector('.name');
 var desc = document.querySelector('.desc');
 var temp = document.querySelector('.temp');
 
+
+
 button.addEventListener('click', function(){
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=16628912310a9e56c80b9573a1903c86&units=metric')
     .then(response => response.json())
@@ -18,12 +20,36 @@ button.addEventListener('click', function(){
         temp.innerHTML = "Temp - " + tempValue;
         desc.innerHTML = "Desc - " + descValue;
     })
-
-
 .catch(err => alert("Invalid city name"))
-
-
-
-
 })
+
+
+
+// START CLOCK SCRIPT
+
+Number.prototype.pad = function(n) {
+    for (var r = this.toString(); r.length < n; r = 0 + r);
+    return r;
+  };
+  
+  function updateClock() {
+    var now = new Date();
+    var min = now.getMinutes(),
+      hou = now.getHours(),
+      mo = now.getMonth(),
+      dy = now.getDate(),
+      yr = now.getFullYear();
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var tags = ["mon", "d", "y", "h", "m"],
+      corr = [months[mo], dy, yr, hou.pad(2), min.pad(2)];
+    for (var i = 0; i < tags.length; i++)
+      document.getElementById(tags[i]).firstChild.nodeValue = corr[i];
+  }
+  
+  function initClock() {
+    updateClock();
+    window.setInterval("updateClock()", 1);
+  }
+  
+  // END CLOCK SCRIPT
 
